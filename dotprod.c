daaffa7339af12befc4d182584a223764ff67fe9
Anthony Kabongo <kabongoa@whitman.edu>
2025-09-29 13:13:18 -0700

    First vector implementation

---
+++ dotprod.c
@@ -0,0 +1,9 @@
+int main(){
+    int dotProduct = 0;
+    int firstVector[3], secondVector[3];
+
+    printf("Enter 3 components of the first vector");
+    scanf("%d %d %d", &firstVector[0], &secondVector[1], &firstVector[2]);
+
+    
+}
